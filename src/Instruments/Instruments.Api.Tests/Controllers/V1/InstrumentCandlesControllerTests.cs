056cbe453b3a3f3e5101c8e1c62a6fc6ada6aaf2
Gergely Reményi <geri@geriremenyi.com>
2020-11-25 22:01:00 +0200

    Adding instrument controller tests

---
+++ src/Instruments/Instruments.Api.Tests/Controllers/V1/InstrumentCandlesControllerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Instruments.Api.Tests.Controllers.V1
+{
+    class InstrumentCandlesControllerTests
+    {
+    }
+}
