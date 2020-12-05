6e4d5bf25617a178635cad0127b1320c2cfe4a58
Gergely Reményi <geri@geriremenyi.com>
2020-12-05 17:05:34 +0100

    Adding Users API deployment

---
+++ scripts/get_version.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Parameters to named parameters
+project_file=$1
+
+# Parameter checks
+if [ "$project_file" = "" ] 
+then
+    echo "[ERROR] No project file given. Please pass a project file name as the first parameter."
+    exit 1
+else 
+    if test ! -f "$project_file"
+    then
+        echo "[ERROR] The given project file '$project_file' doesn't exist."
+        exit 1
+    fi
+fi
+
+# Get version
+grep -oPm1 "(?<=<Version>)[^<]+" <<< "$(cat $project_file)"
